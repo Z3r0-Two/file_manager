b27040415da52d3b6dd8b631974b78becdbb6070
Egor Danilov <gamer.two.work@mail.ru>
2020-07-31 13:21:17 +0300

    Main functionality is read (some errors)

--- file_manager.py
+++ file_manager.py
@@ -1,22 +1,15 @@
 import sys
 import os
 import shutil
-import send2trash
+from send2trash import send2trash
 
 # Store every drive connected on PC in a list
 drives = [chr(x) + ':' for x in range(65, 90) if os.path.exists(chr(x) + ':')]
 
 
-# List each files/folders in the correct working directory
-def list_directories():
-    listdir = os.listdir(os.getcwd())
-    for components in listdir:
-        print('|-' + components)
-
-
-# Select driver that files or folder you need
 def choice_driver():
-    print('\n Chose drive:')
+    # Select driver that files or folder you need
+    print('\nChoose drive:')
 
     print('Drives: ')
     for x in range(len(drives)):
@@ -33,47 +26,260 @@ def choice_driver():
             print('Error\nEnter a correct drive name.\n')
 
 
-# Open file/folders
-def open_component():
-    choice_driver()
-
-    while True:
-        list_directories()
-
-        res = input('\nChoose a file/folder: ')
-        print('\n')
-
-        if res in os.listdir(os.getcwd()):
-            if os.path.isfile(res):
-                os.system('"' + res + '"')
-            else:
-                os.chdir(res)
+def open_component(comp):
+    # Open file/folders
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            os.system('"' + comp + '"')
         else:
-            print('\nNo file/folder with this name')
+            os.chdir(comp)
+    else:
+        print('\nNo file/folder with this name')
 
 
-# Rename files
-def rename():
-    choice_driver()
+def rename(comp):
+    # Rename files
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            comp_name, comp_ext = os.path.splitext(comp)
+            new_title = input('Enter new file name:')
+            new_file_name = '{}{}'.format(new_title, comp_ext)
+            os.rename(comp, new_file_name)
+        if os.path.isdir(comp):
+            new_folder_name = input('Enter new folder name:')
+            os.rename(comp, new_folder_name)
 
-    while True:
-        list_directories()
-        res = input('\nChoose a file/folder: ')
-        print('\n')
 
-        if res in os.listdir(os.getcwd()):
-            if os.path.isfile(res):
-                print('Chosen file:', res)
-                res_name, res_ext = os.path.splitext(res)
-                new_title = input('Enter new file name:')
-                new_file_name = '{}{}'.format(new_title, res_ext)
-                os.rename(res, new_file_name)
-            if os.path.isdir(res):
-                print('Chosen folder:', res)
-                new_folder_name = input('Enter new folder name:')
-                os.rename(res, new_folder_name)
+def move_component(comp):
+    # Move files/folders
+    """
+    We check does is the selected component file or folder
+    Next we select in what drive or folder we want to move file/folder
+    """
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            file_path = os.getcwd() + '\\' + comp
+            print('\nChosen file path:', file_path)
+
+            while True:
+                print('\nDrives:')
+                for x in range(len(drives)):
+                    print(drives[x])
+
+                sel2 = input('\nChoose drive: ')
+                if sel2 in drives:
+                    os.chdir(sel2 + '\\')
+                    drive_path = sel2 + '\\'
+                    print(drive_path)
+                    break
+                else:
+                    print('\nEnter correct drive name')
+
+            while True:
+                print('\nMove component into drive?')
+                ans = input('\nY/N: ')
+                if ans == 'Y':
+                    shutil.move(file_path, drive_path)
+                    break
+                elif ans == 'N':
+                    for files in os.listdir(os.getcwd()):
+                        print('|-' + files)
+
+                    folder = input('\nChoose folder: ')
+                    if folder in os.listdir(os.getcwd()):
+                        if os.path.isdir(folder):
+                            folder_path = os.getcwd() + '\\' + folder
+                        else:
+                            print('\nPlease, select a folder')
+
+                    print('\nMove file into this directory', folder_path, '?')
+                    move = input('\nY/N: ')
+                    if move == 'Y':
+                        shutil.move(file_path, folder_path)
+                        break
+                    elif move == 'N':
+                        os.chdir(sel2)
+                    else:
+                        print('\nPlease enter Y/N')
+
+        elif os.path.isdir(comp):
+            move_folder_path = os.getcwd() + '\\' + comp
+            print('Moving folder path:', move_folder_path)
+
+            while True:
+                print('\nDrives:')
+                for x in range(len(drives)):
+                    print(drives[x])
+
+                sel2 = input('\nChoose drive: ')
+                if sel2 in drives:
+                    os.chdir(sel2 + '\\')
+                    drive_path = sel2 + '\\'
+                    print(drive_path)
+                    break
+                else:
+                    print('\nEnter correct drive name')
+
+            while True:
+                print('\nMove component into drive?')
+                ans = input('\nY/N: ')
+                if ans == 'Y':
+                    shutil.move(move_folder_path, drive_path)
+                    break
+                elif ans == 'N':
+                    for files in os.listdir(os.getcwd()):
+                        print('|-' + files)
+                    folder = input('\nChoose folder: ')
+
+                    if folder in os.listdir(os.getcwd()):
+                        if os.path.isdir(folder):
+                            folder_path = os.getcwd() + '\\' + folder
+                        else:
+                            print('\nPlease, select a folder!')
+
+                    print('\nMove file into this directory', folder_path, '?')
+                    move = input('\nY/N:')
+                    if move == 'Y':
+                        shutil.move(move_folder_path, folder_path)
+                        break
+                    elif move == 'N':
+                        os.chdir(sel2)
+                    else:
+                        print('\nPlease enter Y/N')
+
+
+def copy_component(comp):
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            file_path = os.getcwd() + '\\' + comp
+            print('\nChosen file path:', file_path)
+
+            while True:
+                print('\nDrives:')
+                for x in range(len(drives)):
+                    print(drives[x])
+
+                sel2 = input('\nChoose drive: ')
+                if sel2 in drives:
+                    os.chdir(sel2 + '\\')
+                    drive_path = sel2 + '\\'
+                    print(drive_path)
+                    break
+                else:
+                    print('\nEnter correct drive name')
+
+            while True:
+                print('\nCopy component into drive?')
+                ans = input('\nY/N: ')
+                if ans == 'Y':
+                    shutil.copy(file_path, drive_path)
+                    break
+                elif ans == 'N':
+                    for files in os.listdir(os.getcwd()):
+                        print('|-' + files)
+
+                    folder = input('\nChoose folder: ')
+                    if folder in os.listdir(os.getcwd()):
+                        if os.path.isdir(folder):
+                            folder_path = os.getcwd() + '\\' + folder
+                        else:
+                            print('\nPlease, select a folder')
+
+                    print('\nCopy file into this directory', folder_path, '?')
+                    move = input('\nY/N: ')
+                    if move == 'Y':
+                        shutil.copy(file_path, folder_path)
+                        break
+                    elif move == 'N':
+                        os.chdir(sel2)
+                    else:
+                        print('\nPlease enter Y/N')
+
+        elif os.path.isdir(comp):
+            move_folder_path = os.getcwd() + comp
+            print('Copy folder path:', move_folder_path)
+
+            while True:
+                print('\nDrives:')
+                for x in range(len(drives)):
+                    print(drives[x])
+
+                sel2 = input('\nChoose drive: ')
+                if sel2 in drives:
+                    os.chdir(sel2 + '\\')
+                    drive_path = sel2 + '\\'
+                    print(drive_path)
+                    break
+                else:
+                    print('\nEnter correct drive name')
+
+            while True:
+                print('\nCopy component into drive?')
+                ans = input('\nY/N: ')
+                if ans == 'Y':
+                    shutil.copytree(move_folder_path, drive_path)
+                    break
+                elif ans == 'N':
+                    for files in os.listdir(os.getcwd()):
+                        print('|-' + files)
+                    folder = input('\nChoose folder: ')
+
+                    if folder in os.listdir(os.getcwd()):
+                        if os.path.isdir(folder):
+                            folder_path = os.getcwd() + folder
+                        else:
+                            print('\nPlease, select a folder!')
+
+                    print('\nCopy file into this directory', folder_path, '?')
+                    move = input('\nY/N:')
+                    if move == 'Y':
+                        shutil.copytree(move_folder_path, folder_path)
+                        break
+                    elif move == 'N':
+                        os.chdir(sel2)
+                    else:
+                        print('\nPlease enter Y/N')
+
+
+def send_to_trash(comp):
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            ans = input('Send this file to trash?')
+            if ans == 'Y':
+                send2trash(comp)
+            if ans == 'N':
+                os.chdir('..')
+        if os.path.isdir(comp):
+            ans = input('Send this directory to trash?')
+            if ans == 'Y':
+                send2trash(comp)
+            if ans == 'N':
+                os.chdir('..')
+
+
+def delete_component(comp):
+    if comp in os.listdir(os.getcwd()):
+        if os.path.isfile(comp):
+            ans = input('Delete this file?')
+            if ans == 'Y':
+                os.remove(comp)
+            if ans == 'N':
+                os.chdir('..')
+        if os.path.isdir(comp):
+            ans = input('Delete this folder?')
+            if ans == 'Y':
+                shutil.rmtree(comp)
+            if ans == 'N':
+                os.chdir('..')
 
 
 if __name__ == '__main__':
-    print(drives)
-    rename()
+    choice_driver()
+
+    while True:
+        for file_list in os.listdir(os.getcwd()):
+            print('|-' + file_list)
+
+        file = input('Choose file: ')
+        delete_component(file)
+
